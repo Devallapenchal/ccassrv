a0da17f962963b947bd04eb354ada21d972e6de8
Devallapenchal <37546035+Devallapenchal@users.noreply.github.com>
2018-04-04 02:17:25 +0530

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 public class Hello
 {
 public static void main(String []args)
 {
 System.out.println("Hello world");
 }
-=======
-public class Hello
-{
-public static void main(String []args)
-{
-System.out.println("Hello world");
-}
->>>>>>> dc3bb4e3805541c092ec4045f3e903e9a2449bab
 }
